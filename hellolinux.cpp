f5b39011105689ebad471531656b3edcb111c685
smeredith116 <sebastian.meredith@mymail.champlain.edu>
2019-11-06 12:25:23 -0500

    Hello from the flock

---
+++ hellolinux.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+int main ()
+{
+    vector <string> myChickens=
+    {"Rhode Island Red",
+    "Buff Orpington",
+    "Polish Crested",
+    "Easter Egger"
+    };
+    for(string chicken: myChickens)
+    {
+    cout << chicken << endl;
+    }
+    cout << "All say hello from Linux" << endl;
+    return EXIT_SUCCESS;
+}
+
